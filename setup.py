c02fe051bb4763ad7a4d407e08fd70fb48ae3794
Kibarski Jonathan <phenixdoc@gmail.com>
2013-09-15 11:57:04 +0200

    added dependency PyYAML

--- setup.py
+++ setup.py
@@ -9,11 +9,13 @@ from distutils.core import setup
 
 
 setup(name='git-repo-hooks',
-    version='1.01.001',
+    version='1.01.002',
     description='A tool to manage project, user, and global Git hooks for multiple git repositories',
     author='K Jonathan',
     author_email='phenixdoc@gmail.com',
     url='https://github.com/legion0/git-repo-hooks',
     packages=[],
     scripts=['git-hooks'],
+#	requires=['CommandLineApp (>=2.5)'],
+	requires=['PyYAML'],
     )
